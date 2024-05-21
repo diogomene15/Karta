8478351cffeef323259ee6b00afc47305530435e
diogomene <diogodelimamenezes15@gmail.com>
2024-05-21 19:27:22 -0400

    feat: Adding database controll and bootstrap class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,9 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.room.common)
+    implementation(libs.room.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    annotationProcessor(libs.room.compiler)
 }
